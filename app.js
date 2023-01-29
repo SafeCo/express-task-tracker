a7273c23e5fedaaa2d33039ffbde475ed08d836f
SafeCo <77068178+SafeCo@users.noreply.github.com>
2023-01-29 02:48:55 +0000

    Connected to database

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
 const express = require('express')
 const app = express()
-
 const tasks = require('./routes/tasks')
+const connectDB = require("./db/connect")
+const port = 3000
+require("dotenv").config()
 
 app.use(express.json())
-
 
 app.get("/hello", (req, res)=>{
     res.send("hello Task Manger")
 })
 
-
 app.use('/api/v1/tasks', tasks)
 
+const start = async ()=>{
+    try{
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, () => {console.log(`Example app listening on port ${port}`)})
+    }catch(error){
+        console.log(error)
+    }
+}
 
-const port = 3000
+start()
 
-app.listen(port, () => {console.log(`Example app listening on port ${port}`)})
+
+
--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,15 +1,15 @@
 
 
 const getAllTasks = (req, res) =>{
-    res.send("all items from controller")
+    res.send("get all tasks")
 }
 
 const createTask = (req, res) =>{
-    res.send("create Task")
+    res.json(req.body)
 }
 
 const getTask = (req, res) =>{
-    res.send("get singel task")
+    res.json({id: req.params.id})
 }
 
 const updateTask = (req, res) =>{
