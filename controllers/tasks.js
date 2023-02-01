0c22f2f3a987b6e763e5e5aece44c27c24c0f363
SafeCo <77068178+SafeCo@users.noreply.github.com>
2023-02-01 21:32:12 +0000

    Added delete function

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -36,7 +36,17 @@ const updateTask = (req, res) =>{
     res.send("update task")
 }
 
-const deleteTask = (req, res) =>{
+const deleteTask = async (req, res) =>{
+    try {
+        const {id: taskID} = req.params;
+        const task = await Task.findOneAndDelete({id: taskID})
+        if(!task){
+            return res.status(404).json({msg:`No task with ID: ${taskID} `})
+        }
+        res.status(200).json({task})
+    } catch (error) {
+        
+    }
     res.send("deleteTask")
 }
 
